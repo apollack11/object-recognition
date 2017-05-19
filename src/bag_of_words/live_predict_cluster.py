2185e8d21fd63ba08908431729f19f1795a8f460
Adam Pollack <apollack11@gmail.com>
2017-05-19 16:24:17 -0500

    Clustering for two objects is inconsistent, might return to this later

--- src/bag_of_words/live_predict_cluster.py
+++ src/bag_of_words/live_predict_cluster.py
@@ -12,36 +12,46 @@ cap = cv2.VideoCapture(0)
 # Create SIFT object
 sift = cv2.SIFT()
 
-# Variables for filtering results
-confidences = [0,0,0,0,0]
-counter = 0
-
 needResizing = False
 
 while(True):
-    # Used for filtering results
-    if counter > 4:
-        counter = 0
-
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # List where all the descriptors are stored
-    descriptor_list = []
     kp, des = sift.detectAndCompute(frame, None)
     frame = cv2.drawKeypoints(frame, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) # draw SIFT points
 
-    # descriptors = whiten(descriptors)
+    kp_locations = [keypoint.pt for keypoint in kp]
+    # 
+    # item1_pts = [kp_location for kp_location in kp_locations if kp_location[0] < 213]
+    # item2_pts = [kp_location for kp_location in kp_locations if kp_location[0] > 213 and kp_location[0] < 426]
+    # item3_pts = [kp_location for kp_location in kp_locations if kp_location[0] > 213 and kp_location[0] < 426]
     #
-    # k = 100
-    # vocabulary, variance = kmeans(descriptors, k, 1)
+    # for pt in item1_pts:
+    #     cv2.circle(frame,(int(pt[0]),int(pt[1])), 5, (255,0,0), -1)
+    #
+    # for pt in item2_pts:
+    #     cv2.circle(frame,(int(pt[0]),int(pt[1])), 5, (0,255,0), -1)
+    #
+    # for pt in item3_pts:
+    #     cv2.circle(frame,(int(pt[0]),int(pt[1])), 5, (0,0,255), -1)
+
+
+    test_k = 2
+    clusters, variance = kmeans(whiten(kp_locations), test_k, 1)
+
+    cluster_assignments = vq(whiten(kp_locations), clusters)
+    print len(cluster_assignments[0])
+    print cluster_assignments[0]
+
+    kp_clusters = dict(zip(kp_locations, cluster_assignments[0]))
+
+    for kp_location in kp_clusters.items():
+        cv2.circle(frame,(int(kp_location[0][0]),int(kp_location[0][1])), 5, (255*kp_location[1],0,255), -1)
 
     # Check to make sure descriptor_list has elements
     if des is not None and len(kp) > 15:
-        print "len0",len(des[0])
-        print "len1",len(des[1])
-        print des
-
         test_features = np.zeros((1, k), "float32")
         words, distance = vq(whiten(des), vocabulary)
         for w in words:
@@ -50,12 +60,6 @@ while(True):
 
         # Scale the features
         test_features = std_slr.transform(test_features)
-
-        # predict based on classifier (2 objects)
-        # confidences[counter] = classifier.decision_function(test_features)[0]
-        # counter = counter + 1
-
-        # average_confidence = sum(confidences) / len(confidences)
 
         # predictions based on classifier (more than 2)
         predictions = [class_names[i] for i in classifier.predict(test_features)]
