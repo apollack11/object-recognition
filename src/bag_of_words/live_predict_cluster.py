ef0e523c14bdb88ffc51991481b9c94809decd17
Adam Pollack <apollack11@gmail.com>
2017-05-19 18:44:15 -0500

    Can now find center point of each object

--- src/bag_of_words/live_predict_cluster.py
+++ src/bag_of_words/live_predict_cluster.py
@@ -20,37 +20,23 @@ while(True):
 
     # List where all the descriptors are stored
     kp, des = sift.detectAndCompute(frame, None)
-    frame = cv2.drawKeypoints(frame, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) # draw SIFT points
+    # frame = cv2.drawKeypoints(frame, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) # draw SIFT points
 
-    kp_locations = [keypoint.pt for keypoint in kp]
-    # 
-    # item1_pts = [kp_location for kp_location in kp_locations if kp_location[0] < 213]
-    # item2_pts = [kp_location for kp_location in kp_locations if kp_location[0] > 213 and kp_location[0] < 426]
-    # item3_pts = [kp_location for kp_location in kp_locations if kp_location[0] > 213 and kp_location[0] < 426]
-    #
-    # for pt in item1_pts:
-    #     cv2.circle(frame,(int(pt[0]),int(pt[1])), 5, (255,0,0), -1)
-    #
-    # for pt in item2_pts:
-    #     cv2.circle(frame,(int(pt[0]),int(pt[1])), 5, (0,255,0), -1)
-    #
-    # for pt in item3_pts:
-    #     cv2.circle(frame,(int(pt[0]),int(pt[1])), 5, (0,0,255), -1)
+    pts = [keypoint.pt for keypoint in kp]
+    item_pts = [[pt for pt in pts if pt[0] < 213],[pt for pt in pts if pt[0] > 213 and pt[0] < 426],[pt for pt in pts if pt[0] > 426]]
 
+    for i,item in enumerate(item_pts):
+        xsum = 0
+        ysum = 0
+        for pt in item:
+            xsum += pt[0]
+            ysum += pt[1]
+        avg_item_pt = (xsum/len(item), ysum/len(item))
+        ones = [0,0,0]
+        ones[i] = 1
+        cv2.circle(frame,(int(avg_item_pt[0]),int(avg_item_pt[1])), 5, (255*ones[0],255*ones[1],255*ones[2]), -1)
 
-    test_k = 2
-    clusters, variance = kmeans(whiten(kp_locations), test_k, 1)
-
-    cluster_assignments = vq(whiten(kp_locations), clusters)
-    print len(cluster_assignments[0])
-    print cluster_assignments[0]
-
-    kp_clusters = dict(zip(kp_locations, cluster_assignments[0]))
-
-    for kp_location in kp_clusters.items():
-        cv2.circle(frame,(int(kp_location[0][0]),int(kp_location[0][1])), 5, (255*kp_location[1],0,255), -1)
-
-    # Check to make sure descriptor_list has elements
+    # Check to make sure des has elements and there are more than 15 keypoints
     if des is not None and len(kp) > 15:
         test_features = np.zeros((1, k), "float32")
         words, distance = vq(whiten(des), vocabulary)
